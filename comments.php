39725f218b47183170126b720fc9834fbc4608e6
466548 <t.hara@gmail.com>
2018-04-27 14:07:56 +0900

    コメント一覧を出力

---
+++ comments.php
@@ -0,0 +1,23 @@
+			<div class="box-generic">
+				<div class="box-content box-comment-display">
+					<h2>コメントとトラックバック</h2>
+					<ul class="comment-list">
+					<?php 
+					wp_list_comments( array(
+					'avatar_size' => 130,
+					) );
+					?>						
+					<!-- #comment-## -->
+					</ul>
+				</div>
+			</div>
+			<div class="box-generic">
+				<div class="box-content box-comment-input">
+					<h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
+					<p class="comment-notes">メールアドレスが公開されることはありません。 * が付いている欄は必須項目です</p>
+					<form action="#" class="comment-form">
+						<p class="comment-notes"></p>
+					</form>
+				</div>
+			</div>
+
