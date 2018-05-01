537f98089f49c2291450987b8ccc257c038daf95
466548 <t.hara@gmail.com>
2018-05-01 10:43:32 +0900

    add 404.php

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!-- header切り分け -->
+<?php get_header() ; ?>
+	<div class="page-title">
+		<h1>Not Found</h1>
+	</div>
+	<div class="content-area has-side-col">
+		<div class="main-column">
+			<h1 class="box-heading box-heading-main-col">Not Found</h1>
+			<div class="box-content">
+
+					<p>お探しのページは見つかりませんでした。</p>
+
+			</div>
+
+		
+		</div>
+		
+		<!-- sidebar切り分け -->
+		<?php get_sidebar() ; ?>
+
+	</div>
+
+	<?php get_footer() ; ?>
